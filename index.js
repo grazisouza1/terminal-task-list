e578a83482a7e0565f45baf907b140183c93921f
grazisouza1 <graziisouza0127@gmail.com>
2025-08-26 22:59:50 -0300

    add arrumarIndice function to update task indices in tarefas.json

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ import listaTarefas from "./tarefas.json" assert { type: "json" };
 const salvarTarefa = () => {
   fs.writeFileSync("tarefas.json", JSON.stringify(listaTarefas, null, 2));
 };
+
+const arrumarIndice = () => {
+  if (listaTarefas.length > 0) {
+    listaTarefas.forEach((tarefa, i) => {
+      tarefa.numero = i + 1;
+    });
+  } else {
+    console.log("Nenhuma tarefa encontrada");
+  }
+};
