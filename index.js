65df52a3655e51d5621243eaa566caf4b37b0baf
grazisouza1 <graziisouza0127@gmail.com>
2025-08-26 22:59:13 -0300

    add salvarTarefa function to save tasks to tarefas.json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import inquirer from "inquirer";
 import fs from "fs";
 import listaTarefas from "./tarefas.json" assert { type: "json" };
+
+const salvarTarefa = () => {
+  fs.writeFileSync("tarefas.json", JSON.stringify(listaTarefas, null, 2));
+};
